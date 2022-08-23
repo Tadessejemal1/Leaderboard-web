b585f125af419c2817e1b7053285681f1c3de6ac
Tadesse Jemal <tadessejemal63@gmail.com>
2022-08-23 22:17:35 +0300

    Revert "project-setup"

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles/main.scss';
-import renderAddScore from './modules/domDisplayAddScore.js';
-import renderScore from './modules/domDisplayScores.js';
-
-window.addEventListener('load', () => {
-  renderScore();
-  renderAddScore();
-});
